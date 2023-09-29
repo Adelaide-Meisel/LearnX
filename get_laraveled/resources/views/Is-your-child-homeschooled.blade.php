fe28c7f93d16eaba381b73c3016a2634846bc9e7
Kirby-b <kirby.brodie66@gmail.com>
2023-09-29 08:53:49 -0400

    Update Is-your-child-homeschooled.blade.php

--- get_laraveled/resources/views/Is-your-child-homeschooled.blade.php
+++ get_laraveled/resources/views/Is-your-child-homeschooled.blade.php
@@ -2,9 +2,22 @@
 <!--sign-in page-->
 <link rel="stylesheet" type="text/css" href="/css/app.css">
 
+<body class=new-accounts>
+ 
+   <h1>
+        <div>
+            Create New Account
+        </div>
+    </h1>
 
+    <p id="myParagraph">Is your child(ren) homeschooled? </p>
 
+    <div style="margin-right: 25px; margin-left: 505px; height: 100px; width: 100px; position: absolute">
+        <a type="button" class="Button" value ="Yes"  href={{ route("page.org.at") }}>Yes</a>
+    </div>
+    <div style="margin-left: 25px; margin-left: 897px; height: 100px; width: 100px; position: absolute">
+        <a type="button" class="Button"  value="No" href={{ route("page.are.home.school") }}>No</a>
+    </div>
 
-
-
-
+</body>
+</html>
