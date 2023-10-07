0bc73d36a7abae6c0b36ef68fb8c236944cc4496
kirby-b <kirby.brodie66@gmail.com>
2023-10-07 16:20:19 -0400

    Create Sign-up.blade.php

---
+++ get_laraveled/resources/views/Sign-up.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="/css/app.css">
+
+<div>
+    <p>Please leave your name, your email, and a message to me.<br>(Note this does not actually do anything yet)</p>
+    <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" placeholder="Sample"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" placeholder="Text">
+    </form>  
+    <form>
+        <label for="mail">Email:</label><br>
+        <input type="email" id="mail" name="mail" placeholder="sample@gmail.com"><br>
+    </form>
+    <form>
+        <label for="message">Your message to me:</label><br>
+        <input type="text" id="message" name="message" placeholder="..."><br>
+    </form>
+</div>
+</html>
