96b52fab0cd9f46be188176ab8727a1680dc582e
Kirby-b <kirby.brodie66@gmail.com>
2023-10-11 10:17:06 -0400

    10/11/23
    -Updated donate and what people say
    -Added staff photos

--- get_laraveled/resources/views/donate.blade.php
+++ get_laraveled/resources/views/donate.blade.php
@@ -4,6 +4,22 @@
 
 <body class="faq">
 
-<h1>Give Us Money</h1>
+<div>
+
+<h1>LearnX Can't Do It Alone</h1>
+<p>
+    We apperciate your donation, please type in the amount you wish to donate below:
+</p>
+    <!--put box for $$$ here-->
+
+    Credit/Debit Card #
+    <!--box with format using #-->
+
+    CVC - Three(or four for gift cards and American Express) numbers on the back(or front upper-right corner) of the card
+    <!--box for cvc #-->
+
+    <!--submit button-->
+
+<div>
 
 </body>
--- get_laraveled/resources/views/whatPeopleSay.blade.php
+++ get_laraveled/resources/views/whatPeopleSay.blade.php
@@ -2,18 +2,37 @@
 
 <link rel="stylesheet" type="text/css" href="/css/app.css">
 
-<div class="Text-and-Pic">
-    <div>
-        <img src="/Images/Calculator-On-Notepad.jpg" width="400" height="450" style="float: right;" ><!--DON'T PUT ANYTHING HERE, THIS IS FOR PIC ONLY-->
-    </div>
-    <div style="position: absolute; margin-top: 27px; margin-left: 210px; font-size: 25px; font-family: Arial, Helvetica, sans-serif;">
-        LearnX made learning in school so much easier! -Larua Don<br>
-        <br>
-        School and LearnX equals success. - Kelly Hemp<br>
-    </div>
-    <div style="position: absolute; margin-top: 330px; margin-left: 25px; font-size: 25px; font-family: Arial, Helvetica, sans-serif;">
-        All schools should use LearnX! My child is so successful in school now! -Brooke Asher
-    </div>
-</div>
+    <p>
+        Olivia Smith(K-Student): "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br>
+        Sarah Anderson (Teacher): "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br>
+        Ethan Davis(3rd Grade Student): "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br>
+        James Mitchell (Teacher): "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br>
+        Sophia Roberts (High School Student): "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br>
+        Emily Parker (Teacher): "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br>
+        Ryan Lewis (High School Student): "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br>
+        Michael Turner (Teacher): "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br>
+        Ava Wilson (High School Student): "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br>
+        Lisa Johnson (Teacher): "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br>
+    </p>
 
+    <div class="info-div">
+        <div>
+            <div class="link">
+                <a type="button" value ="Yes" class href={{ route("page.faq") }}>FAQ</a> 
+            </div>
+            <div class="link">
+                <a type="button" value ="Yes" class href={{ route("page.about") }}>ABOUT</a>
+            </div>
+            <div class="link">
+                <a type="button" value ="Yes" class href={{ route("page.why.us") }}>WHY US</a>
+            </div>
+            <div class="link">
+                <a type="button" value ="Yes" class href={{ route("page.donate") }}>DONATE</a>
+            </div>
+            <div class="link">
+                <a type="button" value ="Yes" class href={{ route("page.index") }}>MENU</a>
+            </div>
+            <!--Links to various places on the website-->
+        </div>
+    </d>
 </html>
