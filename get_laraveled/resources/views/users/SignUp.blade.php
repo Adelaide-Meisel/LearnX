bd8be92b57c70cca4e5f88e870e3c14a63f31cf5
Kirby-b <kirby.brodie66@gmail.com>
2023-10-17 10:04:34 -0400

    temporary update
    -temporarily removing some options from sign up until we have a more developed site and are done testing stuff

--- get_laraveled/resources/views/users/SignUp.blade.php
+++ get_laraveled/resources/views/users/SignUp.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <link rel="stylesheet" type="text/css" href="/css/app.css">
-
+<!--f734ff for background color-->
 <div style="position:absolute; margin-right: 25px; margin-left: 505px;">
     <!--Goes to route and then to UserController to be varified and stored-->
     <!--The value "old" makes it so the boxes retain what they say so the user doesnt have 
@@ -32,15 +32,17 @@
             <p style="color:red">{{$message}}</p>
         @enderror
         <!--Confirms password with a custom confirmation name. Errors if you dont put it in or it is incorrect-->
-        <input type="radio" id="choice1" name="radio" value="Org">
+        
+        <!--<input type="radio" id="choice1" name="radio" value="Org">
         <label for="choice1"> I am an organization</label><br>
         <input type="radio" id="choice2" name="radio" value="Teacher">
         <label for="choice2"> I am a teacher</label><br>
         <input type="radio" id="choice3" name="radio" value="Student">
-        <label for="choice3"> I am a student</label><br>
+        <label for="choice3"> I am a student</label><br>-->
         @error('radio')
             <p style="color:red">{{$message}}</p>
         @enderror
+
         <!--Gets your position and errors if you dont pick one-->
         <!--Will add parts next to make it get small errors under the inputs if
         they arent good, along with other features.-->
@@ -48,8 +50,9 @@
         <!--<input type="text" id="orgAt" name="argAt" placeholder="What is your organizations @"><br>
         <input type="text" id="homeSchool" name="homeSchool" placeholder="What is your (or your childs) email"><br>-->
 
-        <label for="message"> What is your purpose for joining</label><br>
-        <textarea name="message" rows="10" cols="30" value="{{old('message')}}"></textarea><br>
+        
+        <!--<label for="message"> What is your purpose for joining</label><br>
+        <textarea name="message" rows="10" cols="30" value="{{old('message')}}"></textarea><br>-->
         <!--Gets the users reason for joining-->
 
         <input type="submit" id="submit" name="submit" value="Create Account">
