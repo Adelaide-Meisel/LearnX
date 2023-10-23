a08ae7b4e96cf506102f7f18d6f8689279ae5ab8
Kirby-b <kirby.brodie66@gmail.com>
2023-10-23 09:41:55 -0400

    meta

--- get_laraveled/resources/views/Gradebook.blade.php
+++ get_laraveled/resources/views/Gradebook.blade.php
@@ -4,8 +4,6 @@
 
     <head>
         <meta charset="UTF-8">
-        <meta name="keywords" content="HTML, CSS, JavaScript, Laravel">
-        <meta name="author" content="Brodie Kirby And Adelaide Meisel">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <h1>
             Ah, yes grade stuff. You have at least a z+
--- get_laraveled/resources/views/home.blade.php
+++ get_laraveled/resources/views/home.blade.php
@@ -3,8 +3,6 @@
 <link rel="stylesheet" type="text/css" href="/css/app.css">
     <head>
         <meta charset="UTF-8">
-        <meta name="keywords" content="HTML, CSS, JavaScript, Laravel">
-        <meta name="author" content="Brodie Kirby And Adelaide Meisel">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <h1>
             Welcome Home. You successfully logged in. Sadly there is nothing yet
@@ -14,6 +12,4 @@
             <input type="submit" id="submit" name="submit" value="Logout">
         </form>
     </head>
-    <body>
-    </body>
 </html>
