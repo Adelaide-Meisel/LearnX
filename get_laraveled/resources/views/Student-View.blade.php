4f5217f0a157a9819917643d06c6843d0edbaa7b
Kirby-b <kirby.brodie66@gmail.com>
2023-11-02 10:02:18 -0400

    Update Student-View.blade.php

--- get_laraveled/resources/views/Student-View.blade.php
+++ get_laraveled/resources/views/Student-View.blade.php
@@ -10,11 +10,14 @@
 
     <div style="border: 2px solid #000; font-family: 'Preahvihear'; font-size: 30px; text-align: left; height: 100px; width: 100%;">
         <div>
-                <div style="position:absolute; margin-left: 200px"><!--Science--><img src="/Images/Science_Icon.jpg" alt="Image cannot load" width="80px" height="90px"><!--Image by rawpixel.com on Freepik>--></div>
+                <div style="position:absolute; margin-left: 200px"><!--Science--><img src="/Images/Science_Icon.jpg" alt="Image cannot load" width="90px" height="100px"><!--Image by rawpixel.com on Freepik>--></div>
                 <div style="position:absolute; margin-left: 350px"><!--Math--><img img src="/Images/Math_Icon.jpg" alt="Image cannot load" width="80px" height="90px"> <!--Image by brgfx on Freepik--></div>
                 <div style="position:absolute; margin-left: 500px"><!--English--><img img src="/Images/English_Icon.jpg" alt="Image cannot load" width="80px" height="90px"><!--Image by upklyak on Freepik--></div>
-                <div style="position:absolute; margin-left: 650px"><!--History--><img img src="/Images/History_Icon.jpg" alt="Image cannot load" width="80px" height="90px"><!--Image by Freepik--></div>
-            LearnX      
+                <div style="position:absolute; margin-left: 650px"><!--History--><img img src="/Images/History_Icon.jpg" alt="Image cannot load" width="80px" height="80px"><!--Image by Freepik--></div>
+                <div style="position:absolute; margin-left: 650px"><!--History--><img img src="/Images/History_Icon.jpg" alt="Image cannot load" width="80px" height="80px"></div>
+                <div></div>
+                <div></div>
+                LearnX      
             <br>
             A+ A+ A+
         </div>
