6e93dd73fb24da13baf8c68043bd14d16bc90277
Kirby-b <kirby.brodie66@gmail.com>
2023-11-10 09:42:57 -0500

    gradebook

--- get_laraveled/resources/views/English.blade.php
+++ get_laraveled/resources/views/English.blade.php
@@ -19,7 +19,7 @@
                 <div style="position:absolute; margin-left: 500px"><!--Math--><a href={{ route("page.math") }}><img src="/Images/Math_Icon.jpg" alt="Image cannot load" width="80px" height="90px"></a><!--Image by brgfx on Freepik--></div>
                 <div style="position:absolute; margin-left: 650px"><!--English--><a href={{ route("page.english") }}><img src="/Images/English_Icon.jpg" alt="Image cannot load" width="80px" height="90px"></a><!--Image by upklyak on Freepik--></div>
                 <div style="position:absolute; margin-left: 800px"><!--History--><a href={{ route("page.history") }}><img src="/Images/History_Icon.jpg" alt="Image cannot load" width="80px" height="80px"></a><!--Image by Freepik--></div>
-                <div style="position:absolute; margin-left: 950px"><!--Grades--><a href={{ route("page.history") }}><img src="/Images/Grade_Icon.jpg" alt="Image cannot load" width="80px" height="80px"></a><!--Image by Freepik--></div>
+                <div style="position:absolute; margin-left: 950px"><!--Grades--><a href={{ route("page.gradebook") }}><img src="/Images/Grade_Icon.png" alt="Image cannot load" width="100px" height="100px"></a><!--Image by Freepik--></div>
                 <div style="position:absolute; margin-left: 85%; margin-top: 3px"><!--pfp--><a href={{ route("page.profile") }}><img src="/Images/Default_Pfp.png" alt="Image cannot load" width="90px" height="90px"></a></div><!--Image by qalebstudio on Freepik-->
                 <div style="position:absolute; margin-left: 93%"><!--Home Button--><a href={{ route("page.home") }}><img src="/Images/Home_Icon.png" alt="Image cannot load" width="90px" height="90px"></a></div><!--Image by qalebstudio on Freepik-->
                 
